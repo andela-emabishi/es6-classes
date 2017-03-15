a6351945e7b269bf511cffef32f2c360ee1f0345
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2017-03-15 10:37:22 +0300

    Add comment

--- super.method.js
+++ super.method.js
@@ -14,7 +14,10 @@ class Animal {
 
 class Monkey extends Animal {
   speaks() {
-    super.speaks(); // super refers to the parent class. super.speaks() calls speaks method in parent class
+    // super refers to the parent class. super.speaks() calls speaks method in parent class
+    // super must be used before this is used as this refers to the instance of the parent class 
+    // and super instantiates the attibutes of the parent class
+    super.speaks();
     return (this.name + ' Screeches');
   }
 }
